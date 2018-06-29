872882e593c54b90b157643a71fc00d2245ec0e2
robomani <robomani@hotmail.com>
2018-06-29 15:36:30 -0400

    d

---
+++ Prog2D/Coroutine et action/Assets/Scripts/MainUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour
+{
+    public Slider m_LifeBar;
+
+    private void Start()
+    {
+        if (GameManager.Instance.m_Player != null)
+        {
+            GameManager.Instance.m_Player.m_HurtAction += SetLifeValue;
+        } 
+    }
+
+
+    private void SetLifeValue(int a_Value)
+    {
+        m_LifeBar.value = a_Value;
+    }
+
+    private void OnDestroy()
+    {
+        if (GameManager.Instance.m_Player != null)
+        {
+            GameManager.Instance.m_Player.m_HurtAction -= SetLifeValue;
+        }
+    }
+}
